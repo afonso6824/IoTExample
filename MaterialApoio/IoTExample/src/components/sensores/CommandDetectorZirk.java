a51a9e82c06ee58f9be2711d7617fbe4664669bc
Bernardo Ribeiro <bernardo.ribeiro98@gmail.com>
2022-12-22 02:32:34 +0000

    Merge "Added analyzers and actuators classes" and  "criaçao do sensor de comando e campainha" commits Co-authored-by: aff6824 <afonsorosa01@gmail.com>

--- MaterialApoio/IoTExample/src/components/sensores/CommandDetectorZirk.java
+++ MaterialApoio/IoTExample/src/components/sensores/CommandDetectorZirk.java
@@ -3,27 +3,46 @@ package components.sensores;
 
 import com.bezirk.middleware.Bezirk;
 import com.bezirk.middleware.java.proxy.BezirkMiddleware;
-import utils.I18N;
 import utils.eventos.ButtonPressedEvent;
 
 import java.util.Scanner;
 
+import utils.I18N;
 import static utils.Messages.DEVICE_RUNNING;
 
 public class CommandDetectorZirk {
     private Bezirk bezirk;
 
-    private Boolean button1Pressed = false;
-    private Boolean button2Pressed = false;
-    private Boolean button3Pressed = false;
-    private Boolean button4Pressed = false;
     private Boolean online = true;
 
-    //TODO ver parte dos aspects na lingua, ou seja como por as coisas a mandarem mensagens no idioma escolhido
+    //TODO ver lingua
+    //TODO falta fazer sair
+    //TODO ver se nao deve ser so comando
     public CommandDetectorZirk() {
         BezirkMiddleware.initialize();
-        bezirk = BezirkMiddleware.registerZirk("Door Bell Detector Zirk");
+        bezirk = BezirkMiddleware.registerZirk("Command Button Detector Zirk");
         System.err.println("Got Bezirk instance");
+    }
+
+    private void processInput(int in) {
+        switch (in) {
+            case 8:
+                online = false;
+                System.err.println("Sensor Stopped");
+                System.exit(0);
+                break;
+            case 9:
+                printMenu();
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                ButtonPressedEvent buttonPressedEvent = new ButtonPressedEvent(in);
+                bezirk.sendEvent(buttonPressedEvent);
+                System.err.println("Published  button pressed update: " + buttonPressedEvent.toString());
+                break;
+        }
     }
 
     private void start() {
@@ -34,81 +53,15 @@ public class CommandDetectorZirk {
         }
     }
 
-    //TODO ver se vale a pena mandar evento periodico && extrair criacao de eventos para nova funcao
-    private void processInput(int in) {
-        switch (in) {
-            case 8:
-                button1Pressed = false;
-                button2Pressed = false;
-                button3Pressed = false;
-                button4Pressed = false;
-                online = false;
-                System.err.println("Sensor Stopped");
-                System.exit(0);
-                break;
-            case 9:
-                printMenu();
-                break;
-            case 0:
-                button1Pressed = true;
-                ButtonPressedEvent buttonPressedEvent = new ButtonPressedEvent();
-                bezirk.sendEvent(buttonPressedEvent);
-                System.err.println("Published open door update: " + buttonPressedEvent.toString());
-                try {
-                    this.wait(500);
-                } catch (InterruptedException e) {
-                    System.err.println("Bell detector couldn't wait 0.5 sec before resetting");
-                }
-                button1Pressed = false;
-                break;
-            case 1:
-                button2Pressed = true;
-                ButtonPressedEvent buttonPressedEvent2 = new ButtonPressedEvent();
-                bezirk.sendEvent(buttonPressedEvent2);
-                System.err.println("Published open door update: " + buttonPressedEvent2.toString());
-                try {
-                    this.wait(500);
-                } catch (InterruptedException e) {
-                    System.err.println("Bell detector couldn't wait 0.5 sec before resetting");
-                }
-                button2Pressed = false;
-                break;
-            case 2:
-                button3Pressed = true;
-                ButtonPressedEvent buttonPressedEvent3 = new ButtonPressedEvent();
-                bezirk.sendEvent(buttonPressedEvent3);
-                System.err.println("Published open door update: " + buttonPressedEvent3.toString());
-                try {
-                    this.wait(500);
-                } catch (InterruptedException e) {
-                    System.err.println("Bell detector couldn't wait 0.5 sec before resetting");
-                }
-                button3Pressed = false;
-                break;
-            case 3:
-                button4Pressed = true;
-                ButtonPressedEvent buttonPressedEvent4 = new ButtonPressedEvent();
-                bezirk.sendEvent(buttonPressedEvent4);
-                System.err.println("Published open door update: " + buttonPressedEvent4.toString());
-                try {
-                    this.wait(500);
-                } catch (InterruptedException e) {
-                    System.err.println("Bell detector couldn't wait 0.5 sec before resetting");
-                }
-                button4Pressed = false;
-                break;
-        }
-    }
-
     private static void printMenu() {
         System.err.println("+************************************************************************************+");
-        System.err.println("* This is a Mock sensor that uses de input values to simulate a real movement input. *");
+        System.err.println("* This is a Mock *"); //TODO fazer depois
         System.err.println("+************************************************************************************+");
         System.err.println("");
-        System.err.println("0 - Press Button one");
-        System.err.println("1 - Press Button two");
-        System.err.println("2 - Press Button three");
-        System.err.println("3 - Press Button four");
+        System.err.println("1 - Press Button SOS");
+        System.err.println("2 - Press Button Lights OFF");
+        System.err.println("3 - Press Button Siren ON");
+        System.err.println("4 - Press Button Open Door");
         System.err.println("8 - Stop sensor");
         System.err.println("9 - Help");
     }
